a13c60b836329cd27ef5c544270dd7c85d65a673
SashaIl <sashailyuk@gmail.com>
2024-10-11 18:37:24 +0200

    added c2

--- class_oop_pr/class_oop_pr.cpp
+++ class_oop_pr/class_oop_pr.cpp
@@ -7,10 +7,16 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-    Car c1;
+    Car c1, c2;
 
     c1.setMark("BMW");
     c1.setModel("x5");
     c1.setYear(2023);
+
+    c2.setMark("Renault");
+    c2.setModel("Megane");
+    c2.setYear(2024);
+
     c1.showCar();
+    c2.showCar();
 }
